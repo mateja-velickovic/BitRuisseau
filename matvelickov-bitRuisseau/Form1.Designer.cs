fae969f1b69b99cf8c876f12b6329cb381c06923
Mateja Velickovic <mateja.velickovic@eduvaud.ch>
2024-11-20 16:29:15 +0100

    feat(app): Initialisation de l'application
    [45min][DONE]

---
+++ matvelickov-bitRuisseau/Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace matvelickov_bitRuisseau
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.upload_media = new System.Windows.Forms.Button();
+            this.file_dialog = new System.Windows.Forms.OpenFileDialog();
+            this.media_list = new System.Windows.Forms.ListView();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.SuspendLayout();
+            // 
+            // upload_media
+            // 
+            this.upload_media.Font = new System.Drawing.Font("Arial", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.upload_media.Location = new System.Drawing.Point(71, 286);
+            this.upload_media.Name = "upload_media";
+            this.upload_media.Size = new System.Drawing.Size(145, 39);
+            this.upload_media.TabIndex = 0;
+            this.upload_media.Text = "Charger un média";
+            this.upload_media.UseVisualStyleBackColor = true;
+            this.upload_media.Click += new System.EventHandler(this.upload_media_Click);
+            // 
+            // file_dialog
+            // 
+            this.file_dialog.FileName = "openFileDialog1";
+            // 
+            // media_list
+            // 
+            this.media_list.HideSelection = false;
+            this.media_list.Location = new System.Drawing.Point(34, 54);
+            this.media_list.Name = "media_list";
+            this.media_list.Size = new System.Drawing.Size(233, 30);
+            this.media_list.TabIndex = 2;
+            this.media_list.UseCompatibleStateImageBehavior = false;
+            // 
+            // imageList1
+            // 
+            this.imageList1.ColorDepth = System.Windows.Forms.ColorDepth.Depth8Bit;
+            this.imageList1.ImageSize = new System.Drawing.Size(16, 16);
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.media_list);
+            this.Controls.Add(this.upload_media);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button upload_media;
+        private System.Windows.Forms.OpenFileDialog file_dialog;
+        private System.Windows.Forms.ListView media_list;
+        private System.Windows.Forms.ImageList imageList1;
+    }
+}
+
---
+++ matvelickov-bitRuisseau/Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.VisualStyles;
+
+namespace matvelickov_bitRuisseau
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Uploading a media
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void upload_media_Click(object sender, EventArgs e)
+        {
+            // Applied filters
+            file_dialog.Filter = "Images et vidéos (*.BMP;*.JPG;*.GIF;*.PNG;*.TIFF;*.MP4;*.AVI;*.MKV;*.MOV;*.WMV)|*.BMP;*.JPG;*.GIF;*.PNG;*.TIFF;*.MP4;*.AVI;*.MKV;*.MOV;*.WMV|" +
+            "All files (*.*)|*.*";
+
+            DialogResult dr = file_dialog.ShowDialog();
+
+            // We check if the user uploaded the file
+            if (dr == DialogResult.OK)
+            {
+                UploadMedia();
+            }
+        }
+
+        /// <summary>
+        /// Function which upload the media in the application
+        /// </summary>
+        public void UploadMedia()
+        {
+            Image.GetThumbnailImageAbort cb = new Image.GetThumbnailImageAbort(CallBack);
+            Bitmap bm = new Bitmap(file_dialog.FileName);
+
+            // Setting the final picture
+            Image final_picture = bm.GetThumbnailImage(300, 300, cb, IntPtr.Zero);
+            imageList1.Images.Add(final_picture);
+            media_list.Items.Add(file_dialog.SafeFileName);
+
+            Graphics theGraphics = Graphics.FromHwnd(this.Handle);
+
+            for (int c = 0; c < imageList1.Images.Count; ++c)
+            {
+                try
+                {
+                    imageList1.Draw(theGraphics, new Point(85, 85 * c / 2), c);
+                }
+                catch (DivideByZeroException)
+                {
+                    imageList1.Draw(theGraphics, new Point(85, 85 * c), c);
+                }
+
+                Application.DoEvents();
+
+                System.Threading.Thread.Sleep(300);
+            }
+        }
+
+        /// <summary>
+        /// Return the callback
+        /// </summary>
+        /// <returns></returns>
+        public bool CallBack()
+        {
+            return false;
+        }
+
+    }
+}
