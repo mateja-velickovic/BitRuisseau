b51bac3aa723373c37aa9e8f510cbafe32453774
Mateja Velickovic <mateja.velickovic@eduvaud.ch>
2024-12-18 13:33:26 +0100

    chore(projet): Importation correct du Backend/Frontend de powercher dans le projet
    [5min][DONE]

---
+++ matvelickov-bitRuisseau/Frontend/BootstrapUI.cs
@@ -0,0 +1,47 @@
+//Auteur : JMY
+//Date   : 28.10.2024 
+//Lieu   : ETML
+//Descr. : Bootstrap pour powercher
+using Backend;
+using Microsoft.Extensions.Logging;
+
+namespace Frontend;
+
+public partial class BootstrapUI : Form
+{
+    MotherNatureUI motherNature;
+    ILoggerFactory loggerFactory;
+    public BootstrapUI(ILoggerFactory loggerFactory)
+    {
+        this.loggerFactory = loggerFactory;
+        InitializeComponent();
+        dpdBroker.SelectedText = "localhost";
+    }
+
+    private void btnMotherNature_Click(object sender, EventArgs e)
+    {
+        if (motherNature == null || motherNature.IsDisposed)
+        {
+            motherNature = new MotherNatureUI(dpdBroker.Text);
+        }
+        motherNature.Show();
+    }
+
+    private void btnBroadcastAgent_Click(object sender, EventArgs e)
+    {
+        HouseUI_mvc houseUI = new HouseUI_mvc(dpdBroker.Text);
+
+        houseUI.Show();
+    }
+
+    private void btnPowerWatch_Click(object sender, EventArgs e)
+    {
+        PowerWatchUI powerWatchUI = new PowerWatchUI(dpdBroker.Text);
+        powerWatchUI.Show();
+    }
+    private void btnPowerDeal_Click(object sender, EventArgs e)
+    {
+        PowerDealUI powerDealUI = new PowerDealUI(dpdBroker.Text);
+        powerDealUI.Show();
+    }
+}
---
+++ matvelickov-bitRuisseau/Frontend/EnergyManager.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing.Text;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataModel;
+using DataModel.Catalog;
+
+namespace Frontend
+{
+    public class EnergyManager
+    {
+        public PCS100_UPS_I UPS = new PCS100_UPS_I("UP29418512", "UPS");
+
+        double currentEnergy = 0;
+        public double _currentEnergy { get => currentEnergy; set => currentEnergy = value; }
+
+    }
+}
